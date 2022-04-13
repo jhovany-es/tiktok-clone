e61cd0349bc91a9355d8b5f440187d0774e7b6da
jhovany <jhovany.escob@gmail.com>
2022-04-12 21:24:03 -0500

    Updates

---
+++ precache-manifest.09fd90f192f1da4590fdaeac2e8a91f9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cc4505db394bcaf00536c9d035257b3d",
+    "url": "/tiktok-clone/index.html"
+  },
+  {
+    "revision": "1fae3147678afd9933a5",
+    "url": "/tiktok-clone/static/css/main.a5ee6a2a.chunk.css"
+  },
+  {
+    "revision": "793ebdf400f4532a959f",
+    "url": "/tiktok-clone/static/js/2.63ba40dc.chunk.js"
+  },
+  {
+    "revision": "3404d1f0cca6416d1e801cf8ac6a4a3c",
+    "url": "/tiktok-clone/static/js/2.63ba40dc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1fae3147678afd9933a5",
+    "url": "/tiktok-clone/static/js/main.ea9f2865.chunk.js"
+  },
+  {
+    "revision": "63495ad83ae69bcb21b2",
+    "url": "/tiktok-clone/static/js/runtime-main.3adf9187.js"
+  }
+]);
